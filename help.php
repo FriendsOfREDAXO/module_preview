2ac4bb361a87a086a760178be95e92fc0734b531
eaCe <webmaster@slaps.de>
2022-12-23 09:11:46 +0100

    chore: update help

--- help.php
+++ help.php
@@ -4,6 +4,11 @@
     <br>
     <br>
     Die Modul-ID gibt den Bildname vor (1.jpg, 2.jpg, etc.).
+    <br>
+    <br>
+    Ist das Theme- und Developer-Addon installiert und die Einstellung "Lade Bilder aus Theme Addon" aktiviert, so werden die Vorschaubilder aus dem Modul-Ordner des Theme-Addons geladen.
+    <br>
+    Dazu muss eine .jpg mit dem Namen <code>module_preview.jpg</code> in das Verzeichniss <code>/private/redaxo/modules/__MODULNAME__</code> gelegt werden.
     <?php
     $file = __DIR__ . '/_changelog.txt';
     if (is_readable($file)) {
